9f0016378fb870561c02f9c82cc06fa1c516a7d0
Brian Faherty <bfaherty@fool.com>
2011-01-05 14:34:23 -0500

    fix for the tests and textmate issues

--- lib/rspec/core/formatters/text_mate_formatter.rb
+++ lib/rspec/core/formatters/text_mate_formatter.rb
@@ -5,13 +5,20 @@ module RSpec
     module Formatters
       # Formats backtraces so they're clickable by TextMate
       class TextMateFormatter < HtmlFormatter
-        def backtrace_line(line)
+        def textmate_backtrace_line(line)
           if line = super(line)
             line.sub!(/([^:]*\.e?rb):(\d*)/) do
               "<a href=\"txmt://open?url=file://#{File.expand_path($1)}&line=#{$2}\">#{$1}:#{$2}</a> "
             end
 
             line
+          end
+
+          def format_backtrace(backtrace, example)
+            return "" unless backtrace
+            return backtrace if example.metadata[:full_backtrace] == true
+            cleansed = backtrace.map { |line| textmate_backtrace_line(line) }.compact
+            cleansed.empty? ? backtrace : cleansed
           end
         end
       end
